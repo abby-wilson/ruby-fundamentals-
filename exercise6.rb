91aaebba5dffc19cf7b902e55450a197c27a5622
Abby Wilson <abigail.wilson1@outlook.com>
2017-06-26 17:15:13 -0400

    exercise 6.2 completed program

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,5 @@
 distance_from_home = 0
+energy = 10
 
 action = "example"
 until action == "go home"
@@ -8,13 +9,24 @@ action = gets.chomp
 
 
 walking_distance = (distance_from_home + 1)
+if (energy < 0) && action == "run"
+  puts "Energy low cannot run"
+  break
+end
+
 if action == "walk"
   distance_from_home += 1
+  energy += 1
   puts "Distance from home is #{distance_from_home}km"
+  puts "Energy at #{energy}"
+
 elsif action == "run"
   distance_from_home += 5
+  energy -= 3
   puts "Distance from home is #{distance_from_home}km"
-else action == !"(go home, walk, run)"
+  puts "Energy at #{energy}"
+
+else action == !"( go home, walk, run )"
   puts "Command does not exist"
 
 end
