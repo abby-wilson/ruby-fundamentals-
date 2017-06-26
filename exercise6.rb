82dfd309031855e0cce731bc0b593780715e8a80
Abby Wilson <abigail.wilson1@outlook.com>
2017-06-26 16:58:05 -0400

    exercise 6 completed program

---
+++ exercise6.rb
@@ -0,0 +1,22 @@
+distance_from_home = 0
+
+action = "example"
+until action == "go home"
+
+puts "Would you like to walk or run?"
+action = gets.chomp
+
+
+walking_distance = (distance_from_home + 1)
+if action == "walk"
+  distance_from_home += 1
+  puts "Distance from home is #{distance_from_home}km"
+elsif action == "run"
+  distance_from_home += 5
+  puts "Distance from home is #{distance_from_home}km"
+
+end
+end
+
+#until gets == "go home"
+#
