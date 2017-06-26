277b79899e74e900399be145231a98ca59645ea6
Abby Wilson <abigail.wilson1@outlook.com>
2017-06-26 14:11:05 -0400

    Completed exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+puts (55 * 0.2)
+
+puts "There are 10 + 20 cars in both parking lots"
+puts "There are #{10 + 20} cars in both parking lots"
+
+puts "There are #{45628 * 7839} species of animals on the planet"
+
+# (10 < 20 TRUE && 30 < 20 FALSE) = FALSE
+# !(10 == 11 FALSE) = TRUE
+# FALSE || TRUE = TRUE
+(10 < 20 && 30 < 20) || !(10 == 11)
