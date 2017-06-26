c13be083a829b8e2d128db4fdc48e4ecb9b4c845
Abby Wilson <abigail.wilson1@outlook.com>
2017-06-26 14:28:04 -0400

    beginning of greeting program

--- exercise2.rb
+++ exercise2.rb
@@ -8,4 +8,4 @@ puts "There are #{45628 * 7839} species of animals on the planet"
 # (10 < 20 TRUE && 30 < 20 FALSE) = FALSE
 # !(10 == 11 FALSE) = TRUE
 # FALSE || TRUE = TRUE
-(10 < 20 && 30 < 20) || !(10 == 11)
+puts (10 < 20 && 30 < 20) || !(10 == 11)
